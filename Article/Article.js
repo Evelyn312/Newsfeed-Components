ebf4017e5a75fd3751709b0b4d75790a0403eb9c
Evelyn Nguyen <Evelyn@Viets-iMacx.local>
2020-04-14 17:34:24 -0700

    added title to page

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,20 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+//step1:
+
+data.forEach((artData) => {
+  let artMaker = document.createElement('div');
+  artMaker.classList.add('article');
+  document.body.appendChild(artMaker);
+
+  let titleMaker = document.createElement('h2');
+  console.log(artData.title);
+  titleMaker.textContent = artData.title;
+  artMaker.appendChild(titleMaker);
+
+  
+
+ 
+});
